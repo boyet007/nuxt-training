30adb8905e907717cf77e26ade10a88b1ca58d34
boyet <boyet007@gmail.com>
2021-05-15 12:51:44 +0700

    13 Nuxt Attribute Pages

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><h1>About</h1></div>
+</template>
+
+<script>
+export default {
+  // load before mount
+  asyncData() {
+    return { title: 'About' }
+  },
+  head() {
+    return {
+      title: 'About',
+      script: [{ src: '/about.js' }],
+    }
+  },
+}
+</script>
