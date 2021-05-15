08325cb41acba40c79d00190832736432b164fdb
boyet <boyet007@gmail.com>
2021-05-15 11:26:15 +0700

    11 Nuxt Custom Error Page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>this is error page</div>
+  <div>{{ error.message }}</div>
 </template>
+
+<script>
+export default {
+  layout: 'errorLayout',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
---
+++ layouts/errorLayout.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>this is error layout</h1>
+    <nuxt />
+  </div>
+</template>
