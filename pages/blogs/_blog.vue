8625a8e662776741836542bc6caeaf666c0dc4af
boyet <boyet007@gmail.com>
2021-05-15 16:47:33 +0700

    16 Route Params Validation

--- pages/blogs/_blog.vue
+++ pages/blogs/_blog.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>I am a blog post</div>
+  <div>
+    I am a blog post
+    <h2>{{ slug.blog }}</h2>
+  </div>
 </template>
+
+<script>
+export default {
+  validate({ params }) {
+    // untuk validasi kalau param harus berupa angka
+    return /^\d+$/.test(params.blog)
+  },
+  data() {
+    return {
+      slug: this.$route.params,
+    }
+  },
+  created() {
+    // console.log(this.$route.params)
+  },
+}
+</script>
